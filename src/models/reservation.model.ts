c229fca1302d8aa36fe1761dcf1795cf1c1d159b
Jonathan <57756132+jdk-21@users.noreply.github.com>
2022-03-20 17:33:49 +0100

    initial commit

---
+++ src/models/reservation.model.ts
@@ -0,0 +1,40 @@
+import {Entity, model, property} from '@loopback/repository';
+
+@model()
+export class Reservation extends Entity {
+  @property({
+    type: 'string',
+    id: true,
+    generated: true,
+  })
+  id?: string;
+
+  @property({
+    type: 'string',
+    required: true,
+  })
+  from: string;
+
+  @property({
+    type: 'string',
+    required: true,
+  })
+  to: string;
+
+  @property({
+    type: 'string',
+    required: true,
+  })
+  room_id: string;
+
+
+  constructor(data?: Partial<Reservation>) {
+    super(data);
+  }
+}
+
+export interface ReservationRelations {
+  // describe navigational properties here
+}
+
+export type ReservationWithRelations = Reservation & ReservationRelations;
