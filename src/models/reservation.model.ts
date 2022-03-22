73f599056d68ed28b073e782bb45aa80841d158e
Jonathan <57756132+jdk-21@users.noreply.github.com>
2022-03-22 09:34:53 +0100

    spcify custom table name -> crud works

--- src/models/reservation.model.ts
+++ src/models/reservation.model.ts
@@ -1,6 +1,13 @@
 import {Entity, model, property} from '@loopback/repository';
 
-@model()
+@model({
+  settings: {
+    postgresql: {
+      // specify custom table name
+      table: 'reservations',
+    },
+  },
+})
 export class Reservation extends Entity {
   @property({
     type: 'string',
