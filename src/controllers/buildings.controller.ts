78ff42f8c75c0b662bb2e726073e0c8ffd6a223b
NaschKadse <klein-dominik@web.de>
2022-03-29 14:15:56 +0200

    authenticate test

--- src/controllers/buildings.controller.ts
+++ src/controllers/buildings.controller.ts
@@ -1,5 +1,6 @@
 /* eslint-disable @typescript-eslint/no-shadow */
 /* eslint-disable @typescript-eslint/naming-convention */
+import {authenticate} from '@loopback/authentication';
 import {inject} from '@loopback/core';
 import {
   Count,
